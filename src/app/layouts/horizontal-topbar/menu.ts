308a55864eff6bff8713b4a1fea99fa8d6390266
andre <suport.info.log@gmail.com>
2026-04-11 11:51:42 -0300

    feat: limpeza do componenent

--- src/app/layouts/horizontal-topbar/menu.ts
+++ src/app/layouts/horizontal-topbar/menu.ts
@@ -1345,34 +1345,6 @@ export const MENU: MenuItem[] = [
           },
         ]
       },
-      {
-        id: 179,
-        label: 'MENUITEMS.MULTILEVEL.TEXT',
-        icon: 'ri-share-line',
-        subItems: [
-          {
-            id: 180,
-            label: 'MENUITEMS.MULTILEVEL.LIST.LEVEL1.1',
-            parentId: 179
-          },
-          {
-            id: 181,
-            label: 'MENUITEMS.MULTILEVEL.LIST.LEVEL1.2',
-            subItems: [
-              {
-                id: 182,
-                label: 'MENUITEMS.MULTILEVEL.LIST.LEVEL1.LEVEL2.1',
-                parentId: 181,
-              },
-              {
-                id: 183,
-                label: 'MENUITEMS.MULTILEVEL.LIST.LEVEL1.LEVEL2.2',
-                parentId: 181,
-              }
-            ]
-          },
-        ]
-      }
     ]
   },
 ];
