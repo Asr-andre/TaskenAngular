ef05e6e5403b434e32a0043e5bc224fbff263b1c
andre <suport.info.log@gmail.com>
2026-04-16 15:25:20 -0300

    feat: ajuste na tela inicial

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+    breadCrumbItems!: Array<{}>;
+
+  ngOnInit(): void {
+    this.breadCrumbItems = [{ label: 'Home', active: true }];
+  }
+}
